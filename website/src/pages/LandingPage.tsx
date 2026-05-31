924d12a9b44e55b574fbb5178bde51c34013a3b8
manthanawgan <manthanawgan@gmail.com>
2026-05-31 21:38:55 +0530

    minor changes

--- website/src/pages/LandingPage.tsx
+++ website/src/pages/LandingPage.tsx
@@ -182,12 +182,12 @@ export default function LandingPage() {
             >
               View on GitHub
             </Link>
-            <Link
+            {/* <Link
               to="/dashboard"
               className="px-5 py-2.5 rounded-button border border-border text-text-secondary text-sm hover:border-text-tertiary hover:text-white transition-all duration-200"
             >
               Dashboard
-            </Link>
+            </Link> */}
           </motion.div>
         </div>
       </section>
